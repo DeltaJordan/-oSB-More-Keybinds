415d9dd29bde42584f7246543e6e4fee5121089e
Jordan Marine <marine.jordan64@gmail.com>
2024-08-09 17:02:07 -0500

    Removed left over debug logging.

--- scripts/morekeybinds/containerhook.lua
+++ scripts/morekeybinds/containerhook.lua
@@ -1,5 +1,4 @@
 function update(dt)
-    sb.logInfo(tostring(input))
     if input then
         if input.bindUp("osb_more_keybinds", "takeAll") then
             world.containerTakeAll(pane.containerEntityId())
