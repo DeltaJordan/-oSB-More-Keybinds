032d1abab4f0dca426116cd23239040ad3e58fac
Jordan Marine <marine.jordan64@gmail.com>
2024-08-09 22:15:28 -0500

    Probably final changes unless I find better ways to interact with containers and player inventory.

--- scripts/morekeybinds/containerhook.lua
+++ scripts/morekeybinds/containerhook.lua
@@ -1,7 +1,30 @@
+local takeAll = function()
+    local containerEntityId = pane.containerEntityId()
+    if not containerEntityId then
+        return
+    end
+
+    local containerItems = world.containerItems(containerEntityId)
+    if not containerItems then
+        return
+    end
+
+    if not player then
+        return
+    end
+
+    for slot, item in pairs(containerItems) do
+        -- Always returns nil, not useful for checking if it worked.
+        local itemConsumed = world.containerConsumeAt(containerEntityId, slot - 1, item.count)
+        player.giveItem(item)
+    end
+end
+
 function update(dt)
     if input then
-        if input.bindUp("osb_more_keybinds", "takeAll") then
-            -- world.containerTakeAll(pane.containerEntityId())
+        -- Use bindHeld until pane input bugs are fixed.
+        if input.bindHeld("osb_more_keybinds", "takeAll") then
+            takeAll()
         end
     end
 end
